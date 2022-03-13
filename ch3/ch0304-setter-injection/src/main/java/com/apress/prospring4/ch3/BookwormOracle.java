0e3c52cdbd24ac126b7b3063a9a9e543a5c17229
albertofullstack <alberto.fullstack@gmail.com>
2022-03-13 21:29:30 +0100

    Añade otras clases de ejemplo de setter injection.

---
+++ ch3/ch0304-setter-injection/src/main/java/com/apress/prospring4/ch3/BookwormOracle.java
@@ -0,0 +1,15 @@
+package com.apress.prospring4.ch3;
+
+public class BookwormOracle implements Oracle {
+
+    private Encyclopedia encyclopedia;
+
+    public void setEncyclopedia(Encyclopedia encyclopedia) {
+        this.encyclopedia = encyclopedia;
+    }
+	
+	public String defineMeaningOfLife() {
+		return "Encyclopedias are a waste of money - go see the world instead";
+	}
+
+}

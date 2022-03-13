595c5738dfd6f85d090bc1ef6f2370740a49a9da
albertofullstack <alberto.fullstack@gmail.com>
2022-03-13 10:25:12 +0100

    Añade implementaciones de provider y render.

---
+++ ch2/ch02-hello-world/src/main/java/com/apress/prospring4/ch2/decoupled/StandardOutMessageRenderer.java
@@ -0,0 +1,25 @@
+package com.apress.prospring4.ch2.decoupled;
+
+public class StandardOutMessageRenderer implements MessageRenderer {
+
+	private MessageProvider messageProvider;
+	
+	@Override
+	public void render() {
+		if (messageProvider == null) {
+			throw new RuntimeException("You must set the property messageProvider of class:" + 
+					StandardOutMessageRenderer.class.getName());
+		}
+		System.out.println(messageProvider.getMessage());
+	}
+
+	@Override
+	public void setMessageProvider(MessageProvider mp) {
+		messageProvider = mp;
+	}
+
+	@Override
+	public MessageProvider getMessageProvider() {
+		return messageProvider;
+	}
+}
